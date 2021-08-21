976c99eaead4a436235c17df5c26fe169648ebea
Khasanov <zkhasanovv@gmail.com>
2021-08-21 17:49:51 +0300

    Add validation for custom input

--- src/components/Common/CustomTextInput.tsx
+++ src/components/Common/CustomTextInput.tsx
@@ -1,25 +1,57 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 interface CustomTextInputInterface {
   placeholder?: string;
   label?: string;
+  value?: string;
+  errorMessage?: string;
+
+  // eslint-disable-next-line react/require-default-props
+  onChange?(e: any): void;
 }
 
 const CustomTextInput: FC<CustomTextInputInterface> = ({
   placeholder,
   label,
+  value,
+  onChange,
+  errorMessage,
 }: CustomTextInputInterface): JSX.Element => {
+  const [inputDirty, setInputDirty] = useState(false);
+
+  const onBlur = () => {
+    setInputDirty(true);
+  };
+
   return (
     <div className="custom-text-input">
       {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <label className="custom-text-input__label">{label}</label>
+      <label
+        className={
+          inputDirty && errorMessage
+            ? "custom-text-input__label custom-text-input__label_error"
+            : "custom-text-input__label"
+        }
+      >
+        {label}
+      </label>
       <div>
         <input
-          className="custom-text-input__input"
+          className={
+            inputDirty && errorMessage
+              ? "custom-text-input__input custom-text-input__input_error"
+              : "custom-text-input__input"
+          }
           type="text"
           placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
         />
       </div>
+      {inputDirty && errorMessage && (
+        <span className="custom-text-input__error-message">{errorMessage}</span>
+      )}
     </div>
   );
 };
@@ -27,6 +59,8 @@ const CustomTextInput: FC<CustomTextInputInterface> = ({
 CustomTextInput.defaultProps = {
   placeholder: "placeholder",
   label: "label",
+  value: "value",
+  errorMessage: "errorMessage",
 };
 
 export default CustomTextInput;
--- src/components/Steps/CarDetailsStep.tsx
+++ src/components/Steps/CarDetailsStep.tsx
@@ -1,8 +1,23 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+// eslint-disable-next-line import/no-named-as-default,import/no-named-as-default-member
 import CustomTextInput from "../Common/CustomTextInput";
 import CarTaxiConfirmation from "../CarTaxiConfirmation";
 
 const CarDetailsStep: FC = (): JSX.Element => {
+  const [email, setEmail] = useState("email");
+  const [emailError, setEmailError] = useState("Ошибочка");
+
+  const emailHandler = (e: any) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(email).toLowerCase())) {
+      setEmailError("Ошибочка");
+    } else {
+      setEmailError("");
+    }
+  };
+
   return (
     <>
       <div className="car-details__car-data">
@@ -12,6 +27,9 @@ const CarDetailsStep: FC = (): JSX.Element => {
             <CustomTextInput
               label="test label"
               placeholder="test placeholder"
+              value={email}
+              errorMessage={emailError}
+              onChange={emailHandler}
             />
             <CustomTextInput />
           </div>
